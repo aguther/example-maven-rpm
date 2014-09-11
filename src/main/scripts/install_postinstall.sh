a755af5f9a4e7eb8a618f1110a76f2c2ef649045
Andreas Guther <andreas@guther.net>
2014-09-11 20:10:22 +0200

    Moved creation of symbolic link from script to rpm instructions

--- src/main/scripts/install_postinstall.sh
+++ src/main/scripts/install_postinstall.sh
@@ -4,8 +4,8 @@
 echo "Processing post-install tasks..."
 
 # create link for environment script in profile.d
-echo "Creating symbolic link for environment script in profile.d ..."
-ln -s /opt/rpm-test/scripts/environment.sh /etc/profile.d/environment.sh
+#echo "Creating symbolic link for environment script in profile.d ..."
+#ln -s /opt/rpm-test/scripts/environment.sh /etc/profile.d/environment.sh
 
 # finished
 echo "Post-install tasks finished."
--- src/main/scripts/install_preremove.sh
+++ src/main/scripts/install_preremove.sh
@@ -4,8 +4,8 @@
 echo "Processing pre-remove tasks..."
 
 # remove symbolic link for envionment script in profile.d
-echo "Removing symbolic link for environment script in profil.d ..."
-rm -f /etc/profile.d/environment.sh
+#echo "Removing symbolic link for environment script in profil.d ..."
+#rm -f /etc/profile.d/environment.sh
 
 # finished
 echo "Pre-remove tasks finished."

ac775a38c322a66a3a0f8699afbb8d0b01b10315
Andreas Guther <andreas@guther.net>
2014-09-11 12:50:38 +0200

    Fixed pre-remove script

--- src/main/scripts/install_preremove.sh
+++ src/main/scripts/install_preremove.sh
@@ -5,7 +5,7 @@ echo "Processing pre-remove tasks..."
 
 # remove symbolic link for envionment script in profile.d
 echo "Removing symbolic link for environment script in profil.d ..."
-rm -f /etc/profile.d/environment
+rm -f /etc/profile.d/environment.sh
 
 # finished
 echo "Pre-remove tasks finished."

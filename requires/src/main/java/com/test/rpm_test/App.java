c1496ba327aeea21368e1a0b693bea26a736c28d
user <user@localhost.localdomain>
2015-10-01 21:31:16 +0200

    Copied to subdirs to test provides/requires feature

---
+++ requires/src/main/java/com/test/rpm_test/App.java
@@ -0,0 +1,13 @@
+package com.test.rpm_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
